1ffb2036461f2e3d8547705ee52e5ab052cdc94b
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2024-09-27 13:57:06 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    screens: {
+      xsm: '350px',
+      sm: '550px',
+      md: '740px',
+      // => @media (min-width: 640px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      '2xl': '1580px',
+
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
