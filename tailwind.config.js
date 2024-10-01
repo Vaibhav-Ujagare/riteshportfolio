4be300c9fec07caf5966765151790e91b80722af
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2024-10-02 00:26:47 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,30 @@ module.exports = {
   content: ["./dist/*.html"],
   theme: {
     screens: {
-      xsm: '350px',
-      sm: '550px',
-      md: '740px',
+      xsm: "350px",
+      sm: "550px",
+      md: "740px",
       // => @media (min-width: 640px) { ... }
 
-      lg: '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      xl: '1280px',
-      '2xl': '1580px',
+      xl: "1280px",
+      "2xl": "1580px",
 
       // => @media (min-width: 1280px) { ... }
     },
     extend: {
-      
-      
+      animation: {
+        "loop-scroll": "loop-scroll 10s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
